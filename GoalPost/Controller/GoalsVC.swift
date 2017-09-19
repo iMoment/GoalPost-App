4ec8263ba13de5dc16144dd16eabf72d439f3acb
Stanley Pan <stanleypan.egre@gmail.com>
2017-09-20 00:10:09 +0900

    GoalsVC built

---
+++ GoalPost/Controller/GoalsVC.swift
@@ -0,0 +1,23 @@
+//
+//  GoalsVC.swift
+//  GoalPost
+//
+//  Created by Stanley Pan on 19/09/2017.
+//  Copyright © 2017 Stanley Pan. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+    
+    //  MARK: Outlets
+    @IBOutlet weak var goalsTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func addGoalButtonPressed(_ sender: UIButton) {
+        
+    }
+}
