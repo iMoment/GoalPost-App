d6e537f9106727b0287e276762580f108844989d
Stanley Pan <stanleypan.egre@gmail.com>
2017-09-20 00:42:25 +0900

    GoalCell File Implemented

---
+++ GoalPost/View/GoalCell.swift
@@ -0,0 +1,23 @@
+//
+//  GoalCell.swift
+//  GoalPost
+//
+//  Created by Stanley Pan on 20/09/2017.
+//  Copyright © 2017 Stanley Pan. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+    
+    //  MARK: Outlets
+    @IBOutlet weak var goalDescriptionLabel: UILabel!
+    @IBOutlet weak var goalTypeLabel: UILabel!
+    @IBOutlet weak var goalProgressLabel: UILabel!
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        self.goalDescriptionLabel.text = description
+        self.goalTypeLabel.text = type
+        self.goalProgressLabel.text = String(describing: goalProgressAmount)
+    }
+}
